5e87dbddc7e43d194d29502d532e5736f32fd4cf
Perseu <josimar.17perseu@gmail.com>
2022-04-07 17:42:59 -0300

    adição de tratamento de entrada

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 let qtnCartas;
 
-function iniciaJogo(){
-    do{
-        qtnCartas = prompt("Escolha quantidade de cartas entre 4 e 14, digite abaixo de 4 para Desistir");
-    }while(qtnCartas%2 === 1 && qtnCartas > 4);
+function iniciaJogo() {
+    do {
+        qtnCartas = prompt("Escolha quantidade de cartas entre 4 e 14");
+    } while ((qtnCartas < 4 || qtnCartas > 14 ) || qtnCartas % 2 === 1 );
+
 
     let lista = document.querySelector(".container");
 
 
-    for(let i=0 ; i<qtnCartas; i++){
+    for (let i = 0; i < qtnCartas; i++) {
         lista.innerHTML += `<article>
-    <img src="img/front.png" alt="">
-    </article>`
+        <img src="img/front.png" alt="">
+        </article>`
     }
+
+
+
 
 }
 
