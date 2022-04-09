d89b2deb6564c394ae08aeda2b2eb77683fa0124
Perseu <josimar.17perseu@gmail.com>
2022-04-09 17:13:07 -0300

    adicionando cartas ao jogo, com movimento de flip em todas e criando vetor para embaralhar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 let qtnCartas;
 
+const vetorCartas = ["img/bobrossparrot.gif", "img/bobrossparrot.gif", "img/explodyparrot.gif", "img/explodyparrot.gif","img/fiestaparrot.gif", "img/fiestaparrot.gif", "img/metalparrot.gif", "img/metalparrot.gif", "img/revertitparrot.gif", "img/revertitparrot.gif", "img/tripletsparrot.gif", "img/tripletsparrot.gif", "img/unicornparrot.gif", "img/unicornparrot.gif" ];
+const vetorEmbaralhado = [];
 function iniciaJogo() {
     do {
         qtnCartas = prompt("Escolha quantidade de cartas entre 4 e 14");
     } while ((qtnCartas < 4 || qtnCartas > 14 ) || qtnCartas % 2 === 1 );
 
-
     let lista = document.querySelector(".container");
 
 
     for (let i = 0; i < qtnCartas; i++) {
+
+        vetorEmbaralhado.push(vetorCartas[i]);
+
         lista.innerHTML += `<div class="carta" onclick=" girarCarta(this)">
 
             <div class = "frente cartaLayout" >
-                frente
+                <img src="../${vetorEmbaralhado[i]}" alt="">
             </div>
             <div class="fundo cartaLayout">
                 <img src="img/front.png" alt="">
