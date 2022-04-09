1dc2aa580936cd2e5bf0f199425dd3aba8cbaa04
Perseu <josimar.17perseu@gmail.com>
2022-04-09 16:22:53 -0300

    adicionando cartas ao jogo, com movimento de flip em todas

--- js/script.js
+++ js/script.js
@@ -10,15 +10,22 @@ function iniciaJogo() {
 
 
     for (let i = 0; i < qtnCartas; i++) {
-        lista.innerHTML += `<article>
-        <img src="img/front.png" alt="">
-        </article>`
+        lista.innerHTML += `<div class="carta" onclick=" girarCarta(this)">
+
+            <div class = "frente cartaLayout" >
+                frente
+            </div>
+            <div class="fundo cartaLayout">
+                <img src="img/front.png" alt="">
+            </div>
+
+        </div>`
     }
 
 }
 
-function girarCarta(){
-    const carta = document.querySelector(".carta");
+function girarCarta(element){
+    const carta = element;
     carta.classList.toggle("flip");
 }
-// iniciaJogo();
+iniciaJogo();
